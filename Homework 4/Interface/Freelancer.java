d1e5f75780cf2bc29054400f64b2aa850a78311c
echamb24 <echamb24@gmail.com>
2026-09-09 22:26:25 -0700

    Begin Interface
    - Defined Payable
    - Made the 2 classes loosely implement to Payable

---
+++ Homework 4/Interface/Freelancer.java
@@ -0,0 +1,11 @@
+public class Freelancer implements Payable{
+    
+
+    public double calculatePayment(){
+        return 0;
+    }
+
+    public String getPayeeName(){
+        return "";
+    }
+}
---
+++ Homework 4/Interface/VendorInvoice.java
@@ -0,0 +1,11 @@
+public class VendorInvoice implements Payable {
+    
+    public double calculatePayment(){
+        return 0;
+    }
+
+    public String getPayeeName(){
+        return "";
+    }
+
+}
